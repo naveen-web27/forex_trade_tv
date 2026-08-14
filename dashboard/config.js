f456dcedb2b36ace21b0b4506f75485045188fae
naveen-web27 <naveenfascino777@gmail.com>
2026-08-14 14:25:49 +0530

    Update config.js

--- dashboard/config.js
+++ dashboard/config.js
@@ -1,5 +1,5 @@
 window.DASHBOARD_CONFIG = {
-  scriptUrl: "",
+  scriptUrl: "https://script.google.com/macros/s/AKfycbyyADs_KVACi-3LFilybTbN_ZcwDTJgLvqnbVxqFFl4q49BbSZxdnpMvUcrpiNyJBLNMg/exec",
   pairs: ["EURUSD", "USDJPY", "GBPUSD", "AUDUSD", "USDCHF", "EURJPY", "GBPJPY", "XAUUSD", "NZDCHF", "USDCAD", "AUDNZD", "AUDCAD"],
   proximityMaxPips: 20
 };
